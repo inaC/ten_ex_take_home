7b8eee0614aa5c545e8a173d1a021ddd5d35b7c0
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2023-07-03 15:27:48 -0300

    feat: add marvel api configuration

---
+++ config/dev.secret.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :ten_ex_take_home, :marvel_api,
+  # insert the public key provided by marvel
+  public_key: "",
+  # insert the private api provided by marvel
+  private_key: ""
