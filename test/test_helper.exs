e2fd7f009416172e879527bbb79f1e07c62a9fed
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2023-07-04 14:36:24 -0300

    test: configura mock para interface de cliente http

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,4 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(TenExTakeHome.Repo, :manual)
+Hammox.defmock(TenExTakeHome.MockHttpClient, for: TenExTakeHome.HttpClient.Behaviour)
+Application.put_env(:ten_ex_take_home, :http_client, TenExTakeHome.MockHttpClient)
